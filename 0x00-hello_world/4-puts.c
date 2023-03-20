beaff0f5b292e899228f334e521c8a40d00f7661
chiomarita <aniezechioma100@gmail.com>
2023-03-19 19:51:29 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - A c program that prints a lines using puts function
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
