67d90071bc3eaf52a911fbc3d0fc140c94d0238e
chiomarita <aniezechioma100@gmail.com>
2023-03-25 03:04:46 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,7 +4,7 @@
  * Description: Prints the numbers excluding 2 and 4
  * Return: The numbers since 0 up to 9
  */
-void print__most_numbers(void)
+void print_most_numbers(void)
 {
 int x = 0;
 for (; x <= 9; x++)
